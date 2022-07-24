3438c25a38ab443042837358159540e33ef8c20f
ZzzGin <13491630+ZzzGin@users.noreply.github.com>
2022-07-24 15:40:02 -0700

    Initial commit

---
+++ test/zzz_img.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ZzzImg from '../lib/zzz_img-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/zzz_img-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ZzzImg.ZzzImgStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
